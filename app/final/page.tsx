31f203f5d9b861ed16a6f9972f5c92f40f1fa3bb
Amarjit-03 <AMARJIT.SINGH39@qmail.cuny.edu>
2024-12-16 01:17:13 +0000

    space change

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -46,7 +46,7 @@ export default function Page() {
 <label htmlFor="service3"> I have a Emergency</label><br></br>
 </div>
 
-
+<br />
 
 
 <button className="ml-10 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" type="submit" >Submit</button>
